9a0777bef02faf25ac5e6039b1f5d9bef8a9a4a0
Ethan Lee <flibitijibibo@gmail.com>
2014-06-17 21:17:41 -0400

    Thanks, static analysis!

--- MonoGame.Framework/Input/Touch/TouchLocation.cs
+++ MonoGame.Framework/Input/Touch/TouchLocation.cs
@@ -188,7 +188,7 @@ namespace Microsoft.Xna.Framework.Input.Touch
 			this.previousPosition = previousPosition;
 			previousPressure = 0.0f;
 
-			timestamp = TimeSpan.FromTicks(DateTime.Now.Ticks);
+			this.timestamp = TimeSpan.FromTicks(DateTime.Now.Ticks);
 			velocity = Vector2.Zero;
 
 			/* If this is a pressed location then store the current position
