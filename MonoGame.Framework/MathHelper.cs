2a70d8a811d124d228218aa75c46d3b38ff96216
Ethan Lee <flibitijibibo@gmail.com>
2014-10-04 23:49:03 -0400

    Move int Clamp to correct region

--- MonoGame.Framework/MathHelper.cs
+++ MonoGame.Framework/MathHelper.cs
@@ -159,27 +159,6 @@ namespace Microsoft.Xna.Framework
 			return value;
 		}
 
-		// FIXME: This could be an extension! ClampIntEXT? -flibit
-		/// <summary>
-		/// Restricts a value to be within a specified range.
-		/// </summary>
-		/// <param name="value">The value to clamp.</param>
-		/// <param name="min">
-		/// The minimum value. If <c>value</c> is less than <c>min</c>, <c>min</c>
-		/// will be returned.
-		/// </param>
-		/// <param name="max">
-		/// The maximum value. If <c>value</c> is greater than <c>max</c>, <c>max</c>
-		/// will be returned.
-		/// </param>
-		/// <returns>The clamped value.</returns>
-		internal static int Clamp(int value, int min, int max)
-		{
-			value = (value > max) ? max : value;
-			value = (value < min) ? min : value;
-			return value;
-		}
-
 		/// <summary>
 		/// Calculates the absolute value of the difference of two values.
 		/// </summary>
@@ -352,6 +331,27 @@ namespace Microsoft.Xna.Framework
 		#endregion
 
 		#region Internal Static Methods
+
+		// FIXME: This could be an extension! ClampIntEXT? -flibit
+		/// <summary>
+		/// Restricts a value to be within a specified range.
+		/// </summary>
+		/// <param name="value">The value to clamp.</param>
+		/// <param name="min">
+		/// The minimum value. If <c>value</c> is less than <c>min</c>, <c>min</c>
+		/// will be returned.
+		/// </param>
+		/// <param name="max">
+		/// The maximum value. If <c>value</c> is greater than <c>max</c>, <c>max</c>
+		/// will be returned.
+		/// </param>
+		/// <returns>The clamped value.</returns>
+		internal static int Clamp(int value, int min, int max)
+		{
+			value = (value > max) ? max : value;
+			value = (value < min) ? min : value;
+			return value;
+		}
 
 		internal static bool WithinEpsilon(float floatA, float floatB)
 		{
